11905c029c97cafd31e814a2aa8306c6462855ad
Burockjohn <brkcn87@gmail.com>
2023-09-04 15:19:21 +0300

    day03 Query/Path parameters

--- src/test/java/com/cydeo/day03/ORDSApiTestsWithParameters.java
+++ src/test/java/com/cydeo/day03/ORDSApiTestsWithParameters.java
@@ -12,7 +12,7 @@ public class ORDSApiTestsWithParameters {
     @BeforeAll
     public static void init() {
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://54.152.219.47:1000/ords/hr";
+        baseURI = "http://54.166.75.122:1000/ords/hr";
     }
 
     /*
