24614b7ab2b4012360cdd27905a3fea4b1820df2
Burockjohn <brkcn87@gmail.com>
2023-09-11 10:51:55 +0300

    HRTestBase updated

--- src/test/java/com/cydeo/utilities/HRTestBase.java
+++ src/test/java/com/cydeo/utilities/HRTestBase.java
@@ -1,5 +1,6 @@
 package com.cydeo.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -8,9 +9,22 @@ public abstract class HRTestBase {
 
     //BeforeAll is an annotation equals to @BeforeClass in testNg, we use with static method name
     @BeforeAll
-    public static void init(){
+    public static void init() {
         //save baseurl inside this variable so that we don't need to type each http method.
         baseURI = "http://54.166.75.122:1000/ords/hr";
+
+        //get ip address from configurations
+        String dbUrl = "jdbc:oracle:thin:@3.216.30.92:1521:xe";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+
+        DBUtils.createConnection(dbUrl, dbUsername, dbPassword);
+    }
+
+    @AfterAll
+    public static void teardown() {
+
+        //DBUtils.destroy();
     }
 
 }
